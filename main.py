623f3594ad7166ca19066629a800ba934ad351a5
OSM Shwa <joshua.guzman.514@gmail.com>
2023-01-15 18:00:42 -0500

    update

--- main.py
+++ main.py
@@ -3,37 +3,48 @@ import os
 run = True
 menu = True
 play = False
+rules = False
 
+# player stats
 HP = 50
 ATK = 3
 
-
+# clear the screen
 def clear():
     os.system("clear")
 
 
+def rules(rules):
+    clear()
+    draw()
+    print("I'm the creator of this game and these are the rules!")
+    draw()
+    input("Press [enter] to return to menu: ")
+    return False
+
+
+# draw lines above the code
 def draw():
     print("xX-------------------------Xx")
 
 
+OPTIONS = ["NEW GAME", "LOAD GAME", "RULES", "QUIT GAME"]
+
+# load a previous game
 def load_game():
     with open("load.txt", "r") as f:
-        load_list = f.readlines()
-        name = load_list[0].strip("\n")
-        HP = load_list[1].strip("\n")
-        ATK = load_list[2].strip("\n")
+        load_list = [n.strip("\n") for n in f.readlines()]
+        print(load_list)
+        # name = load_list[0].strip("\n")
+        # HP = load_list[1].strip("\n")
+        # ATK = load_list[2].strip("\n")
         clear()
-        print(f"Welcome back, {name}")
+        print(f"Welcome back, {load_list[0]}")
 
-    return [name, HP, ATK]
-
-
-def rules():
-    draw()
-    print("I'm the creator of this game and these are the rules!")
-    draw()
+    return load_list
 
 
+# save the game to the load.txt file
 def save_game():
     list = [
         name,
@@ -46,30 +57,30 @@ def save_game():
             f.write(item + "\n")
 
 
+# main game loop
 while run:
     while menu:
         clear()
         draw()
-        print("1. NEW GAME")
-        print("2. LOAD GAME")
-        print("3. RULES")
-        print("4. QUIT GAME")
+        for idx, option in enumerate(OPTIONS, start=1):
+            print(f"{idx}. {option}")
         draw()
 
         choice = input("# ")
 
         if choice == "1":
             clear()
-            name = input("What's your name, hero? ")
+            name = input("# What's your name, hero? ")
             menu = False
             play = True
         elif choice == "2":
             name, HP, ATK = load_game()
-            input(">")
+            input("Press [enter] to continue: ")
             menu = False
             play = True
         elif choice == "3":
-            rules()
+            choice = ""
+            rules(rules)
         elif choice == "4":
             quit()
 
