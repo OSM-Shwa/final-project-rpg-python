6a5a9c9ad348e464eb4ec128e0d955c1d3c817f2
OSM Shwa <joshua.guzman.514@gmail.com>
2023-01-15 16:11:56 -0500

    changes

--- main.py
+++ main.py
@@ -4,36 +4,31 @@ play = False
 rules = False
 
 
-def main():
-    while run:
-        while menu:
-            print("1. NEW GAME")
-            print("2. LOAD GAME")
-            print("3. RULES")
-            print("4. QUIT GAME")
+while run:
+    while menu:
+        print("1. NEW GAME")
+        print("2. LOAD GAME")
+        print("3. RULES")
+        print("4. QUIT GAME")
 
-            if rules:
-                print("I'm the creator of this game and these are the rules!")
-                rules = False
-                choice = ""
-                input("> ")
-            else:
-                choice = input("# ")
+        if rules:
+            print("I'm the creator of this game and these are the rules!")
+            rules = False
+            choice = ""
+            input("> ")
+        else:
+            choice = input("# ")
 
-            if choice == "1":
-                name = input("What's your name, hero? ")
-                menu = False
-                play = True
-            elif choice == "2":
-                pass
-            elif choice == "3":
-                rules = True
-            elif choice == "4":
-                quit()
+        if choice == "1":
+            name = input("What's your name, hero? ")
+            menu = False
+            play = True
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            rules = True
+        elif choice == "4":
+            quit()
 
-        while play:
-            print(name)
-
-
-if __name__ == "__main__":
-    main()
+    while play:
+        print(name)
